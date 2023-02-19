60bc7ee05294fc8999f6493dbb35487936f96424
kuldeep55567 <kanj55567@gmail.com>
2023-02-19 21:01:39 +0530

    Cors fixed again

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config()
 const app = express();
 app.use(express.json())
 app.use(cors({
-    origin:"https://faithful-gold-stockings.cyclic.app"
+    origin:"*"
 }))
 app.get("/", (req, res) => {
     res.send("Home Page")
